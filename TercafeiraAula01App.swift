5bcd86a0fe6da68561c6ceefd001b4638560d60e
Sofia Josafá <sofiajosafa19@gmail.com>
2025-03-11 11:55:15 -0300

    Add files via upload

---
+++ TercafeiraAula01App.swift
@@ -0,0 +1,17 @@
+//
+//  TercafeiraAula01App.swift
+//  TercafeiraAula01
+//
+//  Created by COTEMIG on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TercafeiraAula01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
